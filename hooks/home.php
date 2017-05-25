e868797efbb2b5a3aebc658dc1052a9b854707ed
Sasha Renninger <sashafr@upenn.edu>
2017-05-25 14:12:29 +0000

    add streets to map

--- hooks/home.php
+++ hooks/home.php
@@ -1,13 +1,11 @@
 <?php
 
 
-function HookLeaflet_rsHomeAdditionalheaderjs() {
+function HookLeaflet_rsHomeHomeheader() {
     ?>
 
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.3/dist/leaflet.css"  integrity="sha512-07I2e+7D8p6he1SIM+1twR5TIrhUQn9+I6yjqD53JQjFiMf8EtC93ty0/5vJTZGF8aAocvHYNEDJajGdNx1IsQ==" crossorigin=""/>
 
-    <script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet.js" integrity="sha512-A7vV8IFfih/D732iSSKi20u/ooOfj/AGehOKq0f4vLT1Zr2Y+RX7C+w8A1gaSasGtRUZpF/NZgzSAu4/Gc41Lg==" crossorigin=""></script>    
-    
     <?php
 
 }
@@ -22,14 +20,24 @@ function HookLeaflet_rsHomeHomebeforepanels() {
 
 function HookLeaflet_rsHomeFooterbottom() {
     ?>
-    
+
+    <script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet.js" integrity="sha512-A7vV8IFfih/D732iSSKi20u/ooOfj/AGehOKq0f4vLT1Zr2Y+RX7C+w8A1gaSasGtRUZpF/NZgzSAu4/Gc41Lg==" crossorigin=""></script>
+    <script src="https://unpkg.com/esri-leaflet@2.0.8"></script>
     <script>
-        var map = L.map('leaflet_rs_map').setView([45.526, -122.667], 13);
+        var map = L.map('leaflet_rs_map').setView([39.9526, -75.1652], 13);
 
         L.esri.basemapLayer('Streets').addTo(map);
         L.esri.featureLayer({
             url: 'https://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/Heritage_Trees_Portland/FeatureServer/0'
         }).addTo(map);
+
+        L.geoJSON("eight-days.geojson", {
+            style: function (feature) {
+              return {color: feature.properties.color};
+              }
+            }).bindPopup(function (layer) {
+              return layer.feature.properties.description;
+            }).addTo(map);
     </script>
 <?php
 
