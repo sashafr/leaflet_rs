73330f79211330e24aa99017c2d781e94ecc5697
Sasha Renninger <sashafr@upenn.edu>
2017-06-26 20:31:03 +0000

    submission dropdown styling

--- hooks/all.php
+++ hooks/all.php
@@ -1,4 +1,50 @@
+<?php
+function HookLeaflet_rsAllAddtologintoolbarmiddle() {
+    include "/var/www/resourcespace/include/config.php";
+    ?>
+    <li id = "list">
+          <button id = "submissionsButton" onclick="showDropdown()" class="dropbtn">Submissions</button>
+    </li>
+    <div id="submissions_dropdown" class="dropdown-content" href = "#"></div>
+
+    <script>
+    var baseUrl = "<?php include '/var/www/resourcespace/include/config.php'; echo $baseurl; ?>";
+
+    //create dropdown menu
+    var dropdownElement = document.getElementById("submissions_dropdown");
+    dropdownElement.innerHTML = '<a class="dropdown_option" href="' + baseUrl + '/pages/search.php?search=&archive=-2&resetrestypes=true">Unprocessed<br/></a>';
+    dropdownElement.innerHTML += '<a class="dropdown_option" href="' + baseUrl + '/pages/search.php?search=&archive=-1&resetrestypes=true">Mapped<br/></a>';
+    dropdownElement.innerHTML += '<a class="dropdown_option" href="' + baseUrl + '/pages/search.php?search=&archive=0&resetrestypes=true">Transcribed</a>';
+
+    //hide dropdown initially
+    document.getElementById('submissions_dropdown').style.display = "none";
+
+    function showDropdown() {
+        //if the button is clicked and the dropdown is hidden, show it
+        if (document.getElementById('submissions_dropdown').style.display == "none"){
+            document.getElementById('submissions_dropdown').style.display = "block";
+            return;
+        }
+        //if the button is clicked and the dropdown is visible, hide it
+        if (document.getElementById('submissions_dropdown').style.display == "block"){
+            document.getElementById('submissions_dropdown').style.display = "none";
+            return;
+        }
+    }
+
+    //close dropdown on click outside of dropdown
+    window.onclick = function(event) {
+        if (event.target.id!= 'submissions_dropdown' && event.target.id != 'submissionsButton') {
+            document.getElementById('submissions_dropdown').style.display = "none";
+        }
+    }
+    </script>
+<?php
+} //end hook addlogintoolbarmiddle
+?>
+
 <script>
+//Reload the home page when the user clinks on anything that links to the homepage
 var target;
 document.addEventListener('click', function(e) {
     var target = e.target.href;
@@ -6,37 +52,4 @@ document.addEventListener('click', function(e) {
         window.location.href = "http://45.55.57.30/resourcespace/pages/home.php";
     }
 });
-
-/***************** OTHER OPTIONS FOR RS BACK NAV WORKAROUND *******************/
-/*
-function CentralSpaceLoad () {
-    //alert("central space load");
-    if (target == "http://45.55.57.30/resourcespace/pages/home.php"){
-        window.location.href = "http://45.55.57.30/resourcespace/pages/home.php";
-        return false;
-    }
-}
-
-top.window.onpopstate = function(event) {
-	if (!event.state) {console.log('no event state'); return true;} // no state
-	page = window.history.state;
-	mytitle = page.substr(0, page.indexOf('&&&'));
-	if (mytitle.substr(-1,1) != "'" && mytitle.length! = 0) {
-		page = page.substr(mytitle.length+3);
-		document.title = mytitle;
-		// calculate the name of the page the user is navigating to.
-		pagename = basename(document.URL);
-		pagename = pagename.substr(0, pagename.lastIndexOf('.'));
-		if (pagename == "home") {
-			window.location.href = "http://45.55.57.30/resourcespace/pages/home.php";
-		}
-		ModalClose();
-		jQuery('#CentralSpace').html(page);
-	}
-}
-
-function LoadActions(pagename,id,type,ref) {
-    window.location.href = "http://45.55.57.30/resourcespace/pages/home.php";
-}
-*/
 </script>
