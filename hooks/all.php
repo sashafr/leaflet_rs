2f048a538212fdf89b7ca9259e7092b1dd2cc883
Sasha Renninger <sashafr@upenn.edu>
2017-06-30 21:00:03 +0000

    link checkboxes (nodes)

--- hooks/all.php
+++ hooks/all.php
@@ -11,10 +11,11 @@ function HookLeaflet_rsAllAddtologintoolbarmiddle() {
     var baseUrl = "<?php include '/var/www/resourcespace/include/config.php'; echo $baseurl; ?>";
 
     //create dropdown menu
+
     var dropdownElement = document.getElementById("submissions_dropdown");
-    dropdownElement.innerHTML = '<a class="dropdown_option" href="' + baseUrl + '/pages/search.php?search=&archive=-2&resetrestypes=true">Unprocessed<br/></a>';
-    dropdownElement.innerHTML += '<a class="dropdown_option" href="' + baseUrl + '/pages/search.php?search=&archive=-1&resetrestypes=true">Mapped<br/></a>';
-    dropdownElement.innerHTML += '<a class="dropdown_option" href="' + baseUrl + '/pages/search.php?search=&archive=0&resetrestypes=true">Transcribed</a>';
+    dropdownElement.innerHTML = '<a class="dropdown_option" href="' + baseUrl + '/pages/search.php?search=field94%3AUnprocessed&resetrestypes=true">Unprocessed<br/></a>';
+    dropdownElement.innerHTML += '<a class="dropdown_option" href="' + baseUrl + '/pages/search.php?search=field94%3AMapped&resetrestypes=true">Mapped<br/></a>';
+    dropdownElement.innerHTML += '<a class="dropdown_option" href="' + baseUrl + '/pages/search.php?search=field94%3ATranscribed&resetrestypes=true">Transcribed</a>';
 
     //hide dropdown initially
     document.getElementById('submissions_dropdown').style.display = "none";
@@ -33,6 +34,7 @@ function HookLeaflet_rsAllAddtologintoolbarmiddle() {
     }
 
     //close dropdown on click outside of dropdown
+
     window.onclick = function(event) {
         if (event.target.id!= 'submissions_dropdown' && event.target.id != 'submissionsButton') {
             document.getElementById('submissions_dropdown').style.display = "none";
@@ -51,5 +53,26 @@ document.addEventListener('click', function(e) {
     if (target == "http://45.55.57.30/resourcespace/pages/home.php"){
         window.location.href = "http://45.55.57.30/resourcespace/pages/home.php";
     }
+
 });
+
+//link checkboxes 
+var linkedCheckboxes = { "nodes_349": ["nodes_340", "nodes_346"], "nodes_347": ["nodes_351"]};
+function makeClickCallback(key) {
+  function callback(e){
+    linkedCheckboxes[key].map(function(x){
+      document.getElementById(x).checked = e.target.checked;
+    });
+  }
+  return callback;
+}
+
+if (document.getElementById("question_16")){
+  for (var key in linkedCheckboxes) {
+    document.getElementById(key).addEventListener('click', makeClickCallback(key));
+  }
+}
+
+
+
 </script>
